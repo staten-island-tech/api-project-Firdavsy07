aae9abdc58e96b6740080f7b920ea46f3476ae9c
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-01-06 10:15:00 -0500

    deez nuts

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,11 +1,18 @@
-const URL = "https://api.genderize.io?name=";
-const userName ="peter";
-const gender = `https://api.genderize.io?name=${userName}`;
+const URL = "https://hp-api.onrender.com/api/spells";
 
 async function getData(URL) {
-try{
-const response = await fetch(URL);
-if(response.status < 200 || response.status > 299) {
-}else
+  try {
+    const response = await fetch(URL);
+    if (response.status < 200 || response.status > 299) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.getElementById("api-response").textContent = data.content;
+      console.log("common whalen W");
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("common whalen L");
+  }
 }
-}
+getData(URL);
