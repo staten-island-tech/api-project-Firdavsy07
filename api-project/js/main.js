2e55200aaa5666db8af719217e089ea4f94279c7
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-01-10 10:10:25 -0500

    iate hate js

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,18 +1,22 @@
-const URL = "https://hp-api.onrender.com/api/spells";
+const URL = "https://www.dnd5eapi.co/api/spells";
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     if (response.status < 200 || response.status > 299) {
-      throw new Error(response);
+      console.log(response.status);
+      throw error(response);
     } else {
       const data = await response.json();
-      document.getElementById("api-response").textContent = data.content;
-      console.log("common whalen W");
+      data.data[0].entry.forEach((anime) => {
+        document.getElementById("api-response").insertAdjacentHTML();
+      });
+      console.log(data.data[0].entry[0].title);
     }
   } catch (error) {
     console.log(error);
-    console.log("common whalen L");
+    console.log("L");
+    document.getElementById("api-response").textContent =
+      "Sorry I couldn't find that one";
   }
 }
-getData(URL);
