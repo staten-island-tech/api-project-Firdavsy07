b3a14b5aa206ad06cf83308bf240f4726e7b695f
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-01-13 10:14:30 -0500

    deez nuts deez

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,15 +1,20 @@
 const URL = "https://www.dnd5eapi.co/api/spells";
 
-async function fetchData(URL) {
+async function fetchSpellData(URL) {
   try {
     const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data);
-    console.log("common whalen W");
-    return data;
-  } catch (err) {
-    console.error(err);
+    if (response.status < 200 || response.status > 299) {
+      console.log(response.status);
+      throw error(response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+      console.log("common whalen W");
+      data.index;
+    }
+  } catch (error) {
+    console.log(error);
     console.log("common whalen L");
   }
 }
-fetchData(URL);
+fetchSpellData(URL);
