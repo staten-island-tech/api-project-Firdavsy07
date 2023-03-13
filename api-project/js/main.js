d38b56a30f642c5bb11c5ad6c05d7b791ebadd73
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-03-13 09:46:39 -0400

    Revert "Update main.js"
    This reverts commit 58ac22edb251a81ecb7aa4a7b9b670020ae98047.

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -48,6 +48,7 @@ async function fetchSpellData(URL) {
       });
     }
   } catch (error) {
+<<<<<<< HEAD
     document.getElementById("api-response").insertAdjacentHTML(
       "afterbegin",
       `<div>
@@ -60,4 +61,23 @@ async function fetchSpellData(URL) {
 fetchSpellData();
 DOMselectors.button.addEventListener("click", function () {
   fetchSpellData();
+=======
+    console.log(error);
+    console.log("common whalen L");
+  }
+}
+
+DOMselectors.button.addEventListener("click", function () {
+  console.log(DOMselectors.spellName.value);
+  if (DOMselectors.spellName.value.includes("url")) {
+    fetchSpellData(URL);
+  }
+});
+
+DOMselectors.button.addEventListener("click", function () {
+  console.log(DOMselectors.spellName.value);
+  if (DOMselectors.spellName.value.includes("names")) {
+    fetchSpellName(URL);
+  }
+>>>>>>> parent of 364db45 (i hate apiiii so muchhh)
 });
