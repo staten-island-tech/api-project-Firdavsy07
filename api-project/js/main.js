1f0f07e7c296937a8ae0f285b40d806410e708dc
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-03-13 09:46:50 -0400

    Revert "Update main.js"
    This reverts commit 343f6aa84f6c7071669d545386c55279c098a589.

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,31 +1,9 @@
-const URL = "https://www.dnd5eapi.co/api/spells";
+let URL = "https://www.dnd5eapi.co/api/spells/";
 
 const DOMselectors = {
   spellName: document.querySelector(".spell-name"),
   button: document.getElementById("submit"),
 };
-async function fetchSpellName(URL) {
-  try {
-    const response = await fetch(URL);
-    if (response.status < 200 || response.status > 299) {
-      console.log(response.status);
-      throw error(response);
-    } else {
-      const data = await response.json();
-      console.log(data);
-      console.log("common whalen W");
-      console.log(data.results[1].index);
-      data.results.forEach((data) => {
-        document
-          .getElementById("api-response")
-          .insertAdjacentHTML("afterbegin", `<h2>${data.name}</h2>`);
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    console.log("common whalen L");
-  }
-}
 
 async function fetchSpellData(URL) {
   try {
@@ -38,17 +16,17 @@ async function fetchSpellData(URL) {
       console.log(data);
       console.log("common whalen W");
       console.log(data.results[1].index);
+
       data.results.forEach((data) => {
-        document
-          .getElementById("api-response")
-          .insertAdjacentHTML(
-            "afterbegin",
-            `<h2>https://www.dnd5eapi.co/api/spells/${data.index}</h2>`
-          );
+        document.getElementById("api-response").insertAdjacentHTML(
+          "afterbegin",
+          `<div>
+            <h2>${data.name}</h2>
+            </div>`
+        );
       });
     }
   } catch (error) {
-<<<<<<< HEAD
     document.getElementById("api-response").insertAdjacentHTML(
       "afterbegin",
       `<div>
@@ -61,23 +39,4 @@ async function fetchSpellData(URL) {
 fetchSpellData();
 DOMselectors.button.addEventListener("click", function () {
   fetchSpellData();
-=======
-    console.log(error);
-    console.log("common whalen L");
-  }
-}
-
-DOMselectors.button.addEventListener("click", function () {
-  console.log(DOMselectors.spellName.value);
-  if (DOMselectors.spellName.value.includes("url")) {
-    fetchSpellData(URL);
-  }
-});
-
-DOMselectors.button.addEventListener("click", function () {
-  console.log(DOMselectors.spellName.value);
-  if (DOMselectors.spellName.value.includes("names")) {
-    fetchSpellName(URL);
-  }
->>>>>>> parent of 364db45 (i hate apiiii so muchhh)
 });
